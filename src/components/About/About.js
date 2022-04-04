d430530523f2229be915c89a059159eeb3c58499
rezainiet <masudrezaog@gmail.com>
2022-04-05 04:59:27 +0600

    error 404 page implement complete

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const About = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1 className='text-5xl font-bold text-center'>This is about page!</h1>
+        <div >
+            <h1 className='text-5xl font-bold text-center my-5'>This is about page!</h1>
+            <div className='flex justify-center items-center py-20'>
+                <button onClick={() => navigate('/')} className='bg-purple-500 px-3 py-2 rounded-md'>Back to Home</button>
+            </div>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h2>This is Not Found Page!</h2>
+        <div className='flex justify-center items-center py-20'>
+            <img src="error-404.png" alt="Not Found" />
+            <button onClick={() => navigate('/')} className='bg-purple-500 px-3 py-2 rounded-md'>Back to Home</button>
         </div>
     );
 };
